afc6bb0086cbc16c5bb9f628b4082cdf807eb42c
rachione <lychnussys@gmail.com>
2022-12-09 21:42:30 -0500

    add: blood

--- src/UI.h
+++ src/UI.h
@@ -13,21 +13,26 @@ public:
     UI(vector<Shader *> &s)
         : shaders(s)
     {
-        sprite = new Sprite();
-        sprite->Set("UI/blood1_b.png", vec2(0), vec2(0));
+        uiframe = new Sprite();
+        uiframe->Set("UI/blood1_b.png", vec2(0), vec2(0));
+
+        blood= new Sprite();
+        blood->Set("UI/blood2.png", vec2(0), vec2(0));
     };
     ~UI()
     {
-        delete sprite;
+        delete uiframe;
     };
     void Draw()
     {
 
         shaders[4]->Use();
-        sprite->Draw();
+        blood->Draw();
+        uiframe->Draw();
+        
     }
 
 private:
     vector<Shader *> &shaders;
-    Sprite *sprite;
+    Sprite *uiframe,*blood;
 };
